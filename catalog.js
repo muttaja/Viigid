61a9ba57ea75c34b4bef629975e29e561231f4db
muttaja <44384093+muttaja@users.noreply.github.com>
2026-03-17 00:33:46 +0200

    Create catalog.js

---
+++ catalog.js
@@ -0,0 +1,26 @@
+fetch("figs.json")
+.then(res => res.json())
+.then(data => {
+
+const body = document.getElementById("figBody")
+
+data.forEach(fig => {
+
+const row = document.createElement("tr")
+
+row.innerHTML = `
+<td>${fig.name}</td>
+<td>${fig.varasus}</td>
+<td>${fig.maitsev}</td>
+<td>${fig.suurus}</td>
+<td>${fig.kulmakindlus}</td>
+<td>${fig.varvus}</td>
+<td>${fig.breba}</td>
+<td>${fig.saadavus}</td>
+`
+
+body.appendChild(row)
+
+})
+
+})
