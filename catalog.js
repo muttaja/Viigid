2cbc0d825e0cbbcd6aae434a46bf1fa12c10a613
muttaja <44384093+muttaja@users.noreply.github.com>
2026-03-17 22:27:30 +0200

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,22 +1,23 @@
 let filterKandnud = true
 let filterSaadavus = true
-
 let figsData = []
 
 function renderTable(data){
 
-const body = document.getElementById("figBody")
-body.innerHTML = ""
+const body=document.getElementById("figBody")
+body.innerHTML=""
 
-data.forEach(fig => {
+data.forEach(fig=>{
 
 if(filterKandnud && fig.kandnud !== "jah") return
 if(filterSaadavus && fig.saadavus !== "jah") return
 
-const row = document.createElement("tr")
+const row=document.createElement("tr")
+
+let photoIcon = fig.image ? " 📷" : ""
 
 row.innerHTML = `
-<td><a href="variety.html?name=${encodeURIComponent(fig.name)}">${fig.name}</a></td>
+<td><a href="variety.html?name=${encodeURIComponent(fig.name)}">${fig.name}</a>${photoIcon}</td>
 <td>${fig.varasus}</td>
 <td>${fig.maitsev}</td>
 <td>${fig.suurus}</td>
@@ -24,7 +25,6 @@ row.innerHTML = `
 <td>${fig.varvus}</td>
 <td>${fig.breba}</td>
 <td>${fig.riik}</td>
-<td>${fig.kandnud}</td>
 <td>${fig.saadavus}</td>
 `
 
@@ -35,32 +35,10 @@ body.appendChild(row)
 }
 
 fetch("figs.json")
-.then(res => res.json())
-.then(data => {
+.then(res=>res.json())
+.then(data=>{
 
 figsData = data
 renderTable(figsData)
 
 })
-
-document.getElementById("kandnudBtn").onclick = () => {
-
-filterKandnud = !filterKandnud
-
-document.getElementById("kandnudBtn").innerText =
-"Kandnud: " + (filterKandnud ? "ON" : "OFF")
-
-renderTable(figsData)
-
-}
-
-document.getElementById("saadavusBtn").onclick = () => {
-
-filterSaadavus = !filterSaadavus
-
-document.getElementById("saadavusBtn").innerText =
-"Saadaval: " + (filterSaadavus ? "ON" : "OFF")
-
-renderTable(figsData)
-
-}
